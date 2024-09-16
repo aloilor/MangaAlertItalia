4f200e61f3b02eb7a901d1b85da44719ce3dd976
aloilor <aloisi.lorenzo99@gmail.com>
2024-09-16 13:55:31 +0200

    logging configuration update

--- src/backend/manga-scraper/main.py
+++ src/backend/manga-scraper/main.py
@@ -1,10 +1,16 @@
 from utils.logging_config import setup_logging
 from manga_scraper_app import MangaScraperApp
+import logging
+
+logger = logging.getLogger(__name__)
+
 
 def main():
     setup_logging()
+    logger.info(f"Starting to scrape...")
     app = MangaScraperApp()
     app.scrape_and_notify()
+    logger.info(f"Scrape ended...")
 
 if __name__ == "__main__":
     main()
--- src/backend/manga-scraper/utils/logging_config.py
+++ src/backend/manga-scraper/utils/logging_config.py
@@ -5,5 +5,5 @@ def setup_logging():
     logging_level = os.environ.get('LOGGING_LEVEL', 'INFO').upper()
     logging.basicConfig(
         level=logging_level,
-        format='%(asctime)s - %(name)s - %(levelname)s - %(message)s'
+        format='%(asctime)s - %(name)s - %(levelname)s - %(funcName)s - %(message)s '
     )
