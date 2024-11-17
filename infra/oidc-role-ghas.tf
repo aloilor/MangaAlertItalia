2c5f3192ee32ae24e21225cab1222ab603b9f3e7
aloilor <aloisi.lorenzo99@gmail.com>
2024-11-17 22:35:51 +0100

    Add some policies to the oidc ghas role

--- infra/oidc-role-ghas.tf
+++ infra/oidc-role-ghas.tf
@@ -34,21 +34,126 @@ resource "aws_iam_role" "github_actions_role" {
       },
     ],
   })
-  managed_policy_arns = [
-    "arn:aws:iam::aws:policy/AWSOrganizationsFullAccess",
-    "arn:aws:iam::aws:policy/AmazonDynamoDBFullAccess",
-    "arn:aws:iam::aws:policy/AmazonEC2FullAccess",
-    "arn:aws:iam::aws:policy/AmazonRDSFullAccess",
-    "arn:aws:iam::aws:policy/AmazonS3FullAccess",
-    "arn:aws:iam::aws:policy/IAMFullAccess",
-    "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryFullAccess",
-    "arn:aws:iam::aws:policy/AmazonECS_FullAccess",
-    "arn:aws:iam::aws:policy/AmazonEventBridgeFullAccess",
-    "arn:aws:iam::aws:policy/AmazonSESFullAccess"
-  ]
+  managed_policy_arns   = [aws_iam_policy.ghas_admin_policy.arn]
   max_session_duration  = 3600
   force_detach_policies = false
 }
 
+# Custom policy for (almost) admin access
+resource "aws_iam_policy" "ghas_admin_policy" {
+  name        = "github-actions-combined-policy"
+  description = "Combined policy with access to commonly used AWS services for GitHub Actions."
+
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+
+      # Amazon EC2, ECS and ECR access
+      {
+        Effect = "Allow",
+        Action = [
+          "ec2:*",
+          "ecs:*",
+          "ecr:*",
+        ],
+        Resource = "*"
+      },
+
+      # Amazon S3 access
+      {
+        Effect = "Allow",
+        Action = [
+          "s3:*",
+        ],
+        Resource = "*"
+      },
+
+      # Amazon RDS access
+      {
+        Effect = "Allow",
+        Action = [
+          "rds:*",
+        ],
+        Resource = "*"
+      },
+
+      # Amazon DynamoDB access
+      {
+        Effect = "Allow",
+        Action = [
+          "dynamodb:*",
+        ],
+        Resource = "*"
+      },
+
+      # AWS Lambda access
+      {
+        Effect = "Allow",
+        Action = [
+          "lambda:*",
+        ],
+        Resource = "*"
+      },
+
+      # Amazon CloudWatch access
+      {
+        Effect = "Allow",
+        Action = [
+          "cloudwatch:*",
+          "logs:*",
+          "events:*",
+        ],
+        Resource = "*"
+      },
+
+      # AWS IAM access (limited to necessary actions)
+      {
+        Effect = "Allow",
+        Action = [
+          "iam:*"
+        ],
+        Resource = "*"
+      },
+
+      # Amazon SES access
+      {
+        Effect = "Allow",
+        Action = [
+          "ses:*",
+        ],
+        Resource = "*"
+      },
+
+      # AWS Organizations access
+      {
+        Effect = "Allow",
+        Action = [
+          "organizations:*",
+        ],
+        Resource = "*"
+      },
+
+      # AWS Route53 access
+      {
+        Effect = "Allow",
+        Action = [
+          "route53:*",
+        ],
+        Resource = "*"
+      },
+
+      # AWS Cloudfront access
+      {
+        Effect = "Allow",
+        Action = [
+          "cloudfront:*",
+        ],
+        Resource = "*"
+      },
+
+
+    ]
+  })
+}
 
 
